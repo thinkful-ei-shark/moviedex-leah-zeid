8b8716f129562a36891f197140b723c7f745fc1e
Leah Arden <leaharden175@gmail.com>
2020-10-20 16:58:10 -0400

    finished

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ require('dotenv').config()
 const movieData = require('./movies-data.js');
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors')
+const helmet = require('helmet')
 
 
 const app = express();
-app.use(morgan('common'));
+app.use(morgan('common'))
+app.use(helmet())
+app.use(cors())
+
 
 app.use(function validateBearerToken(req, res, next) {
     //info will go here
